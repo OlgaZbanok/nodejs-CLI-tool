b7638301dfe325913e9ec54ae45b89d74c53c631
Olga Zbanok <olga@zbanok.com>
2020-03-25 00:22:06 +0300

    feat: implement ceaser cipher algorithm

---
+++ task1/ceaserCipher.js
@@ -0,0 +1,17 @@
+module.exports.ceaserCipher = function ceaserCipher(str, amount, isEncode) {
+  const shift = isEncode ? amount : (26 - amount) % 26;
+
+  return String.fromCharCode(
+    ...str.split('').map(char => {
+      let code = char.charCodeAt();
+      if (char.match(/[a-zA-Z]/i)) {
+        if (code >= 65 && code <= 90) {
+          code = ((code - 65 + shift) % 26) + 65;
+        } else if (code >= 97 && code <= 122) {
+          code = ((code - 97 + shift) % 26) + 97;
+        }
+      }
+      return code;
+    })
+  );
+};
