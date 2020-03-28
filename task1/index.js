d420dc1d7ca8b17263814e331e8e8f3e2bb4055c
Olga Zbanok <olga@zbanok.com>
2020-03-29 00:18:14 +0300

    refactor: optimize code

--- task1/index.js
+++ task1/index.js
@@ -1,49 +1,29 @@
-const cipher = require('./ceaserCipher');
-const { Command } = require('commander');
-
+const program  = require('./commands');
+const transform = require('./transform');
 const fs = require('fs');
 const path = require('path');
-const { Transform, pipeline } = require('stream');
+const { pipeline } = require('stream');
 const { promisify } = require('util');
-
-const program = new Command();
-
-program.storeOptionsAsProperties(false);
-
-program
-  .requiredOption('-s, --shift <code>', 'a shift')
-  .requiredOption('-a, --action <type>', 'an action encode/decode')
-  .option('-i, --input <inputfile>', 'an input file')
-  .option('-o, --output <outputfile>', 'an output file');
-
-program.parse(process.argv);
-const programOptions = program.opts();
-
-const caeserCipherEncode = new Transform({
-  transform: (chunk, encoding, done) => {
-    const result = cipher.ceaserCipher(
-      chunk.toString(),
-      +programOptions.shift,
-      programOptions.action === 'encode'
-    );
-    done(null, result);
-  }
-});
 
 const pipelineAsync = promisify(pipeline);
 
-const inputStream = programOptions.input
-  ? fs.createReadStream(path.join(__dirname, programOptions.input))
+const inputStream = program.input
+  ? fs.createReadStream(path.join(__dirname, program.input))
   : process.stdin;
-const outputStream = programOptions.output
-  ? fs.createWriteStream(path.join(__dirname, programOptions.output))
+const outputStream = program.output
+  ? fs.createWriteStream(path.join(__dirname, program.output), { flags:'a' })
   : process.stdout;
 
 (async function doCeaserCipher() {
   try {
-    await pipelineAsync(inputStream, caeserCipherEncode, outputStream);
+    await pipelineAsync(
+      inputStream,
+      transform,
+      outputStream);
+
     console.log('Encoding succeeded');
   } catch (err) {
     console.error(`Encoding failed with error ${err}`);
+    process.exit(1);
   }
 }());
