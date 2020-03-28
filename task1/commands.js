c28a2523add72af7e2cf7ff13581801379f25b79
Olga Zbanok <olga@zbanok.com>
2020-03-29 00:12:53 +0300

    feat: add argument checking

--- task1/commands.js
+++ task1/commands.js
@@ -1,6 +1,7 @@
 const { Command } = require('commander');
 
 try {
+    const ACTIONS = ['encode', 'decode'];
     const program = new Command();
     program
         .requiredOption('-s, --shift <number>', 'a shift')
@@ -8,6 +9,15 @@ try {
         .option('-i, --input <inputfile>', 'an input file')
         .option('-o, --output <outputfile>', 'an output file');
 
+    program.parse(process.argv);
+
+    if (isNaN(program.shift)) {
+        throw new Error('Shift parameter is not valid!');
+    }
+
+    if (!ACTIONS.includes(program.action.toString())) {
+        throw new Error('Action parameter is not valid!');
+    }
 
     module.exports = program;
 } catch (error) {
